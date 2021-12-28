710c8232ee25529957dca6ccc5d5c1c86297351c
BaoNguyen <nguyenlehb1808@gmail.com>
2021-12-28 13:22:25 +0700

    user model+controller+service

---
+++ server/routes/account.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as controller from "../controllers/account.controller.js";
+
+const accountRoute = express.Router();
+
+accountRoute.post("/logIn", controller.logIn);
+accountRoute.post("/signUp", controller.signUp);
+accountRoute.get("/me", controller.getInfo);
+
+export default accountRoute;
